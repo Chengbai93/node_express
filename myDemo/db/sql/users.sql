76218c7054a62b5a59946bc1225e6f4cb35463c8
XuDS <xudongsheng@swhysc.com>
2024-03-05 13:22:22 +0800

    add login jwt handler

---
+++ myDemo/db/sql/users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile_phone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
